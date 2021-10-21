f117eab9f91dc88ea3a00f17e3cd535451c30fac
Tanguy CAMUS <tanguy.camus2021@campus-eni.fr>
2021-10-21 11:28:51 +0200

    first commit

---
+++ src/main/java/com/formation/eni/gestionPotager/GestionPotagerApplication.java
@@ -0,0 +1,13 @@
+package com.formation.eni.gestionPotager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPotagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPotagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/formation/eni/gestionPotager/GestionPotagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation.eni.gestionPotager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPotagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
