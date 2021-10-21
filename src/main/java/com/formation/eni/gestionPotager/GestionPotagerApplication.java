71a7a28a1f3fefe21afb18917ff99ecfb2036a19
Thibaut Kerouedan <thibaut.kerouedan2021@campus-eni.fr>
2021-10-21 14:25:24 +0200

    CREATE : test data dans '...Application'

--- src/main/java/com/formation/eni/gestionPotager/GestionPotagerApplication.java
+++ src/main/java/com/formation/eni/gestionPotager/GestionPotagerApplication.java
@@ -1,13 +1,38 @@
 package com.formation.eni.gestionPotager;
 
+import java.time.LocalDate;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.formation.eni.gestionPotager.bo.Activity;
+import com.formation.eni.gestionPotager.bo.Field;
+import com.formation.eni.gestionPotager.bo.Implentation;
+import com.formation.eni.gestionPotager.bo.Lieu;
+import com.formation.eni.gestionPotager.bo.Plant;
+import com.formation.eni.gestionPotager.bo.Potager;
+
 @SpringBootApplication
-public class GestionPotagerApplication {
+public class GestionPotagerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionPotagerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		Lieu potager = new Potager();
+		
+		Activity act = new Activity(LocalDate.of(2022, 23, 5), "La kermes du potager !", potager);
+		
+		Field carre = new Field();
+		
+		Implentation plantePlantee = new Implentation();
+		
+		Plant plante = new Plant();
+		
 	}
 
 }
