584f78d7a2168dda9efb5a1b93ac67e9994d0e48
Thibaut Kerouedan <thibaut.kerouedan2021@campus-eni.fr>
2021-10-21 13:35:44 +0200

    CREATE : 'Activity' components bo/dal

--- src/main/java/com/formation/eni/gestionPotager/bo/Activity.java
+++ src/main/java/com/formation/eni/gestionPotager/bo/Activity.java
@@ -1,5 +1,26 @@
 package com.formation.eni.gestionPotager.bo;
 
-public class Activity {
+import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Activity {
+	@Id
+	@GeneratedValue
+	private Integer idActivity;
+	private LocalDate date;
+	private String evenement;
+	@ManyToOne
+	private Lieu lieu;
 }
